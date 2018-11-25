de0a754f822f241e075cade603bdd6d039513e3a
Hxmusic <hxmusic@foxmail.com>
2018-11-25 15:36:19 +0800

    modify the title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,26 @@
 # coding: utf-8
 module ApplicationHelper
+  SYSTITLE="Rofala"
   def full_title(page_title = '')
     
   # 根据所在的页面返回完整的标题 def full_title(page_title = '')
-    base_title = "FOXEXY XERP for Amazon"
+    base_title = SYSTITLE+" XERP for Amazon"
     if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
     end
+  end
+
+  def sys_title(title = '')
+    
+    otitle=SYSTITLE
+    if title.empty?
+      otitle
+    else
+      otitle=title
+    end
+        
   end
 
 
